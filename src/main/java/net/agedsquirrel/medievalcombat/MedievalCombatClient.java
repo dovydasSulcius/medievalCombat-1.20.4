0ffeaaf70e182222e45501ccc805ff352e890bb5
Dovydas Sulcius <sulcius.dovydas@gmail.com>
2024-02-12 01:04:47 +0200

    Initial commit yay look a new mod

---
+++ src/main/java/net/agedsquirrel/medievalcombat/MedievalCombatClient.java
@@ -0,0 +1,10 @@
+package net.agedsquirrel.medievalcombat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MedievalCombatClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/agedsquirrel/medievalcombat/MedievalCombatDataGenerator.java
@@ -0,0 +1,11 @@
+package net.agedsquirrel.medievalcombat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MedievalCombatDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
