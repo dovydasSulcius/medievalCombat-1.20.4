42a50069d8369d887466ecfe106e60df6e88ec2d
Dovydas Sulcius <sulcius.dovydas@gmail.com>
2024-02-12 15:24:41 +0200

    YEY 1 day by now

--- src/main/java/net/agedsquirrel/medievalcombat/MedievalCombatDataGenerator.java
+++ src/main/java/net/agedsquirrel/medievalcombat/MedievalCombatDataGenerator.java
@@ -1,11 +1,18 @@
 package net.agedsquirrel.medievalcombat;
 
+import net.agedsquirrel.medievalcombat.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class MedievalCombatDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
--- src/main/java/net/agedsquirrel/medievalcombat/datagen/ModRecipeProvider.java
+++ src/main/java/net/agedsquirrel/medievalcombat/datagen/ModRecipeProvider.java
@@ -5,9 +5,12 @@ import net.agedsquirrel.medievalcombat.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 
@@ -23,5 +26,15 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         offerBlasting(exporter, STEEL_SMETLING, RecipeCategory.MISC, ModItems.STEEL_INGOT, 10, 2000, "steel");
 
         offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.STEEL_INGOT, RecipeCategory.MISC, ModBlocks.STEEL_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.STEEL_BLOCK, 1)
+                .pattern("SSS")
+                .pattern("SMS")
+                .pattern("SSS")
+                .input('S', ModItems.STEEL_INGOT)
+                .input('M', Items.END_CRYSTAL)
+                .criterion(hasItem(ModItems.STEEL_INGOT), conditionsFromItem(ModItems.STEEL_INGOT))
+                .criterion(hasItem(Items.END_CRYSTAL), conditionsFromItem(Items.END_CRYSTAL))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.STEEL_BLOCK)));
     }
 }
